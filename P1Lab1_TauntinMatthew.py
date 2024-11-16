bbc3878669e5322770f59bbaf39bbee79fa28c2f
MT-Student-Faytech <tauntonm0063@student.faytechcc.edu>
2024-11-16 10:36:32 -0500

    Add files via upload

---
+++ P1Lab1_TauntinMatthew.py
@@ -0,0 +1,9 @@
+# Matthew E. Taunton
+# 11/14/2024
+# P1Lab1
+# Says hello to you after you entire your name.
+
+firnam = input('Enter your first name: ')
+lasnam = input('Enter your last name: ')
+print('Hello, ' + firnam + ' ' + lasnam + '! Welcome to CTI-110')
+
